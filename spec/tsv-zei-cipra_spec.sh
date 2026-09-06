ee222399980d7a8d38eea787baa3f054bd6086c9
qq542vev <qq542vev@yahoo.co.jp>
2026-09-06 22:47:26 +0900

    test: enhance TSV validation and error reporting

--- spec/tsv-zei-cipra_spec.sh
+++ spec/tsv-zei-cipra_spec.sh
@@ -14,9 +14,9 @@
 ##
 ##   id - 37bd1ed6-ff1e-4795-ae87-8de577d3a682
 ##   author - <qq542vev at https://purl.org/meta/me/>
-##   version - 1.0.0
+##   version - 1.1.0
 ##   created - 2026-04-10
-##   modified - 2026-04-10
+##   modified - 2026-09-06
 ##   copyright - Copyright (C) 2026-2026 qq542vev. All rights reserved.
 ##   license - <GPL-3.0-only at https://www.gnu.org/licenses/gpl-3.0.txt>
 ##   depends - awk, find
@@ -31,15 +31,45 @@ eval "$(shellspec -) exit 1"
 
 Describe '.i lo tsv zei vreji cu te cipra'
 	tsv_zei_cipra() {
-		awk -F '\t' -- '
+		LANG=C awk -F '\t' -- '
+			function fliba(xi, ciksi) {
+				printf("fliba %d %d: %s : [%s]\n", NR, xi, ciksi, $xi)
+				jalge = 1
+			}
+
 			BEGIN {
 				morna = "^([a-gi-pr-vx-z]|([aeiou](\047[aeiou])+))+$"
+				kmorna = "\343\201[\201-\277]|\343\202[\200-\237]|\343\203\274"
 				jalge = 0
 			}
 
-			NF != 3 || $1 !~ morna || $2 == "" || $3 == "" {
-				printf("fliba %d: [%s]\n", NR, $0)
+			NF != 3 {
+				printf("fliba %d: na cimei : [%s]\n", NR, $0)
 				jalge = 1
+				next
+			}
+
+			$1 !~ morna {
+				fliba(1, "na lojbo valsi")
+			}
+
+			$2 == "" {
+				fliba(2, "ciksi fo no da")
+			}
+
+			$3 !~ "^(" kmorna ")+(,(" kmorna ")+)*$" {
+				fliba(3, "na me la xiraganas")
+			}
+
+			{
+				terkancu = split($3, kana, ",")
+
+				for(xi = 1; xi < terkancu; xi++) {
+					if(kana[xi + 1] <= kana[xi]) {
+						fliba(3, "me lo te porsi")
+						break
+					}
+				}
 			}
 
 			END { exit jalge }
